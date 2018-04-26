e2221969dfa92a390370560ec1a5e9a37615953e
Winster332 <stas-martynov-98@mail.ru>
2018-04-26 12:41:22 +0300

    refactoring

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Timers;
-using System.Net.Http;
-using System.Net;
-using System.Net.Sockets;
-using System.IO;
+using RadiusVision.RVParser;
+using RadiusVision.Mjpeg;
 
 namespace WpfApp3
 {
